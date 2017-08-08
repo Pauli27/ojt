fb247f33cb498392c74855c142d88df513640349
zedmaniquiz <zed.maniquiz10@gmail.com>
2017-08-08 13:08:29 +0800

    zed

--- zed/inventory/inventory_edit.php
+++ zed/inventory/inventory_edit.php
@@ -15,8 +15,6 @@ if(isset($_GET['Id']) && !empty($_GET['Id'])){
 	$inventorydata = $db->select_inventory_where($id);
 }
 
-
-
 ?>
 <html>
 
@@ -26,8 +24,7 @@ if(isset($_GET['Id']) && !empty($_GET['Id'])){
 <body>
 <div id="inventory_form">
  <?php //echo var_dump($inventorydata);die();?>
-	<form name="inventory" method="POST" action="inventory_edit.php?Id=<?=$inventorydata['Id']?>">
-	<pre>
+	<form name="inventory" method="POST" onsubmit="return ConfirmEdit()">
 	<div>Item Name: <input type="text" name="itemname" value="<?=$inventorydata['itemname']?>"></div>
 	<div>Description: <input type="text" name="desc" value="<?=$inventorydata['description']?>"></div>
 	<div>Qty: <input type="text" name="qty" value="<?=$inventorydata['quantity']?>"></div>
@@ -35,9 +32,22 @@ if(isset($_GET['Id']) && !empty($_GET['Id'])){
 	<input type='hidden' name='id' value="<?=$inventorydata['Id']?>">
 		<div> <input type="submit" name="submit" value="Submit" ></div>
 		<div id="admin"><a href='zed_exercise3.php'>Back</a></div>
-	</pre>
+	
 </form>
 </div>
+<script>
+	function ConfirmEdit(){
+    if (confirm("Edit account?")){
+    	alert('Sucess');
+          return true;
+
+    }
+    else {
+       alert('Fail to edit');
+       return false;
+    }
+}  
+</script>
 
 </body>
 </html>
