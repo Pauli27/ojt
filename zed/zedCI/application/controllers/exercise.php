d5ade96c2e78a71cacdf3f0809458988ac9f3a17
zedmaniquiz <zed.maniquiz10@gmail.com>
2017-08-18 15:22:20 +0800

    asa

--- zed/zedCI/application/controllers/exercise.php
+++ zed/zedCI/application/controllers/exercise.php
@@ -3,16 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Exercise extends CI_Controller {
 
-	
-	public function index()
-	{	
-		$this->load->model('exercise_model');
-		// $haha = array();
-		// echo "<pre>",print_r($this->db),"</pre>";die();
-		$haha = $this->exercise_model->get();
-		// echo $this->db->last_query();die();
 
-		 // echo print_r($haha);
-		 $this->load->view('exercises');
-	}
+function index()
+{
+$this->load->view('exercises');// loading form view
+}
+function form_validation(){
+	echo 'OK';
+		 
+}
+
 }
--- zed/zedCI/application/models/exercise_model.php
+++ zed/zedCI/application/models/exercise_model.php
@@ -1,37 +1,13 @@
-<?php
-class Exercise_model extends CI_Model{
-
-	public function __construct(){
-		parent::__construct();
-	}
-	public function get(){
-		// $this->db->trans_start();
-			$this->db->select('*');
-			$this->db->from('mvc3');
-		// $aasa = ("SELECT * from 'users' ");
-		// echo $aasa;die();
-			// $this->db->COUNT('*');
-			// if($id != null){
-			// 	$this->db->where('user_roles.id',$id);
-			// }
-			// if($exclude_admin)
-			// 	$this->db->where('user_roles.id !=', 1);
-			// $this->db->order_by('id desc');
-			$query = $this->db->get();
-			$result = $query->result();
-		// $this->db->trans_complete();
-		return $result;
-	}
-	// public function add_user_roles($items){
-	// 	$this->db->insert('user_roles',$items);
-	// 	$x=$this->db->insert_id();
-	// 	return $x;
-	// }
-	// public function update_user_roles($user,$id){
-	// 	$this->db->where('id', $id);
-	// 	$this->db->update('user_roles', $user);
-
-	// 	return $this->db->last_query();
-	// }
+<?php 
+class crud_model extends CI_Model
+{
+function insert_into_db()
+{
+$fn = $_POST['firstname'];
+$ln = $_POST['lastname'];
+$un = $_POST['username'];
+$pw = $_POST['password'];
+$this->db->query("INSERT INTO mvc3 VALUES('$fn','$ln','$un','$pw')");
+}
 }
 ?>
